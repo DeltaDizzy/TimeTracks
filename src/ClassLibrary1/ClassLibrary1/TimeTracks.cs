7659fbfdb928c6af3bffcbee5c61c1f7d914e9ae
DeltaDizzy <EJBraswell@gmail.com>
2018-06-30 22:40:20 -0400

    h

---
+++ src/ClassLibrary1/ClassLibrary1/TimeTracks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TimeTracks
+{
+    public class TimeTracks :MonoBehaviour
+    {
+        public void Log(string msg)
+        {
+
+        }
+        public void JumpTrack()
+        {
+
+        }
+    }
+}

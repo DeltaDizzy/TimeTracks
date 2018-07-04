72ab71a446a0484f4892132efcb07457ab779216
DeltaDizzy <EJBraswell@gmail.com>
2018-07-04 15:05:14 -0500

    y

---
+++ src/TimeTracks/ClassLibrary1/TimeTracks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TimeTracks
+{
+    public class TimeTracks :MonoBehaviour
+    {
+        KerbalEVA kerb;
+        Collider collider;
+
+        private void Awake()
+        {
+            collider = gameObject.GetComponent<Collider>();
+        }
+        public void Log(string msg)
+        {
+            msg = String.Format(msg, "[TIMETRACKS]: ");
+            Debug.Log(msg);
+        }
+        public void JumpTrack()
+        {
+            collider.enabled = false;
+        }
+        public void Arrive()
+        {
+            collider.enabled = true;
+        }
+    }
+}
